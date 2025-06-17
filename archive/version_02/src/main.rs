93481bddb10d1ac7080400d9d9e9879d9d9936f8
Raymond Perminov <raymond@brizoit.com>
2025-06-17 18:09:15 +0300

    added example for GTK4 about 'Message from Rust' and update archive

---
+++ archive/version_02/src/main.rs
@@ -0,0 +1,22 @@
+use libloading::{Library, Symbol};
+use std::{ffi::CString, os::raw::c_char};
+
+fn main() {
+    //             "./native/libMacUIBridge.dylib";
+    let lib_path = "./native/libGtkUIBridge.so";
+
+    let lib = unsafe {
+        Library::new(lib_path)
+            .expect("Failed to load dylib")
+    };
+
+    let message = CString::new("Hello from Rust! Raymond 🎯").unwrap();
+
+    unsafe {
+        let show_window_with_text: Symbol<unsafe extern "C" fn(*const c_char)> =
+            lib.get(b"show_window_with_text")
+               .expect("❌ Function show_mac_window not found");
+
+        show_window_with_text(message.as_ptr());
+    }
+}
